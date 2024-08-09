81955ca4bec8a9cf34e877019b7905d354781f71
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2024-08-10 01:22:28 +0200

    create footer component with content

---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <footer>
+      <h3>Get in touch at cleopapadopoulou@gmail.com</h3>
+      <hr />
+      <p>
+        Designed by Kleopatra Emmanouil, developed by Dimosthenis Emmanouil
+        @2024{' '}
+      </p>
+    </footer>
+  );
+};
