3338016c094e5351195f1932604caf070a64c98a
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2024-08-09 23:52:48 +0200

    add active state styling and import new fonts for the li links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,43 @@
+'use client';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { Work_Sans } from 'next/font/google';
 import React from 'react';
 
+const workSans = Work_Sans({
+  subsets: ['latin'],
+  weight: ['700'],
+});
+
+interface LinkItem {
+  id: number;
+  name: string;
+  url: string;
+}
+
+const Links: LinkItem[] = [
+  {
+    id: 1,
+    name: 'Work',
+    url: '/work',
+  },
+  {
+    id: 2,
+    name: 'About',
+    url: '/about',
+  },
+  {
+    id: 3,
+    name: 'My resume',
+    url: '/resume',
+  },
+];
+
 const Navbar = () => {
+  const pathname = usePathname();
+
   return (
-    <nav className='flex justify-between px-20 items-center h-[88px] text-[#3D56D9] text-xl'>
+    <nav className='flex justify-between px-20 items-center h-[88px] text-[#3D56D9] text-xl bg-white'>
       <div>
         <Link
           href={'/'}
@@ -14,15 +48,20 @@ const Navbar = () => {
       </div>
       <div>
         <ul className='flex items-center gap-4'>
-          <li>
-            <Link href={'/'}>Work</Link>
-          </li>
-          <li>
-            <Link href={'/'}>About</Link>
-          </li>
-          <li>
-            <Link href={'/'}>My resume</Link>
-          </li>
+          {Links.map((link) => (
+            <li key={link.id} className={workSans.className}>
+              <Link
+                href={link.url}
+                className={`p-2 ${
+                  pathname === link.url
+                    ? 'border border-[#3D56D9] px-3 rounded-sm'
+                    : ''
+                }`}
+              >
+                {link.name}
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
     </nav>
