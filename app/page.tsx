9cc1aa697a6fb7499942ef6bc34e1512fdc1982d
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2024-08-12 19:41:42 +0200

    fix styling for responsivness

--- app/page.tsx
+++ app/page.tsx
@@ -4,27 +4,15 @@ export default function Home() {
   return (
     <main className='flex justify-center items-center flex-col mt-10 gap-8'>
       <section>
-        <h1 className='text-lg'>Hello! I'm Kleopatra Emmanouil I, research</h1>
+        <h1 className='text-base text-[#3D56D9] md:text-xl font-semibold leading-8 px-3'>
+          Hello! I'm Kleopatra I, research
+        </h1>
       </section>
       <section>
         <figure className='flex flex-col gap-4'>
           <div className='relative group cursor-pointer'>
             <Image
               src='/case.png'
-              height={400}
-              width={580}
-              alt='picture of an apple laptop'
-              className='group-hover:opacity-75 transition-opacity duration-300'
-            />
-            <div className='absolute inset-0 flex justify-center items-center bg-black/75 opacity-0 group-hover:opacity-100 transition-opacity duration-300'>
-              <span className='text-white text-xl font-bold w-[133px] text-center'>
-                SEB Nexus 2024
-              </span>
-            </div>
-          </div>
-          <div className='relative group cursor-pointer'>
-            <Image
-              src='/case2.png'
               height={400}
               width={580}
               alt='picture of an apple laptop'
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className='flex flex-col mx-auto gap-10 mb-5 max-w-[1169px] sticky top-[100vh]'>
       <div>
-        <h3 className='mt-10 mb-5 text-[#3D56D9] text-2xl font-bold'>
+        <h3 className='mt-10 mb-5 text-[#3D56D9] text-xl md:text-2xl text-cneter font-bold'>
           Get in touch at cleopapadopoulou@gmail.com
         </h3>
         <hr className='border-t-2 border-[#3D56D9] w-full' />
