90b2fcfa717cea739fb7d0b01a02af589d53ed4b
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2024-08-12 20:35:28 +0200

    fix some details on styling

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,27 +1,30 @@
+// app/about/page.tsx
 import Image from 'next/image';
 import Link from 'next/link';
+import { workSans } from '../layout'; // Import the workSans font variable
 
 export default function About() {
   return (
-    <main className='flex flex-col max-w-[1169px] mx-auto text-[#3D56D9] h-full p-4 md:p-0'>
+    <main className='flex flex-col w-[90%] max-w-[1169px] mx-auto text-[#3D56D9] h-full p-4 md:p-0'>
       <div>
         <h1 className='text-start text-2xl mx-3 mb-6'>About Me</h1>
       </div>
       <section className='flex flex-col md:h-[536px] md:flex-row justify-center md:gap-0 md:mx-3 lg:gap-10'>
-        <figure className='flex-shrink-0 w-full max-w-full md:h-full md:max-w-[370px] lg:max-w-[470px] lg:max-h-[536px]'>
+        <figure className='flex-shrink-0 w-full max-w-full md:max-w-[370px] lg:max-w-[470px] md:h-full lg:max-h-[536px]'>
           <Image
-            className='w-full object-cover'
+            className='w-full h-full object-cover'
             src='/IMG_1848.JPG'
             width={470}
             height={536}
-            sizes='100vw'
             alt='A picture of Kleopatra with a smile on her face'
           />
         </figure>
-        <article className='flex flex-col gap-6 md:gap-10 md:ml-12'>
+        <article
+          className={`flex flex-col gap-6 md:gap-10 md:ml-5 ${workSans.variable}`}
+        >
           <div>
-            <h2 className='my-3 md:my-0'>Hello!</h2>
-            <p className='font-medium'>
+            <h2 className='my-3 mb-5'>Hello!</h2>
+            <p>
               I am a UX/UI designer based in Stockholm. Passionate about
               understanding user needs and crafting exceptional interfaces. With
               a focus on continuous growth and a user-centric approach, I place
@@ -29,10 +32,10 @@ export default function About() {
               captivating and delightful experiences.
             </p>
           </div>
-          <div className='mt-4 md:mt-10'>
+          <div className='mt-4 md:mt-10 w-full'>
             <Link
               href='#'
-              className='bg-[#3D56D9] w-full md:w-auto max-w-[306px] rounded-2xl text-white text-center p-2 px-4 font-thin'
+              className='bg-[#3D56D9] w-full md:w-auto max-w-[306px] rounded-2xl text-white text-center p-2 px-3 md:px-[1px] md:py-2 lg:p-2 lg:px-3 font-thin'
             >
               Connect with me on LinkedIn!
             </Link>
