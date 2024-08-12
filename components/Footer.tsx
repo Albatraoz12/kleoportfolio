e498b377b64f9603386c519cea5d677cc0f2b4f0
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2024-08-12 20:35:37 +0200

    fix responsiveness

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className='flex flex-col mx-auto gap-10 mb-5 max-w-[1169px] sticky top-[100vh]'>
+    <footer className='flex flex-col mx-auto w-[90%] gap-10 mb-5 lg:max-w-[888px] md:text-center xl:max-w-[1169px] sticky top-[100vh]'>
       <div>
         <h3 className='mt-10 mb-5 text-[#3D56D9] text-xl md:text-2xl text-cneter font-bold'>
           Get in touch at cleopapadopoulou@gmail.com
