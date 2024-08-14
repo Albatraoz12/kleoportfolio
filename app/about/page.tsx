fdf85041525e8f34d49e56b5c69f823622f0d5c7
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2024-08-14 14:48:19 +0200

    fix responsiveness text on about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,14 +1,22 @@
+import { Work_Sans } from 'next/font/google';
 import Image from 'next/image';
 import Link from 'next/link';
 
+const workSans = Work_Sans({
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
+});
+
 export default function About() {
   return (
-    <main className='flex flex-col w-[90%] max-w-[1169px] mx-auto text-[#3D56D9] h-full p-4 md:p-0'>
-      <div>
-        <h1 className='text-start text-2xl mx-3 mb-6'>About Me</h1>
+    <main
+      className={`flex flex-col w-full max-w-[1169px] mx-auto text-[#3D56D9] h-full p-4 md:p-0 lg:p-0 lg:w-[889px] ${workSans.className}`}
+    >
+      <div className='mb-16'>
+        <h1 className='text-start text-2xl mx-3'>About Me</h1>
       </div>
-      <section className='flex flex-col md:h-[536px] md:flex-row justify-center md:gap-0 md:mx-3 lg:gap-10'>
-        <figure className='flex-shrink-0 w-full max-w-full md:max-w-[370px] lg:max-w-[470px] md:h-full lg:max-h-[536px]'>
+      <section className='flex flex-col items-center gap-7 mb-20 md:h-[536px] md:flex-row justify-center md:gap-0 md:mx-3 lg:gap-10 lg:justify-between'>
+        <figure className='flex-shrink-0 w-[255px] h-[375px] lg:w-full max-w-full md:max-w-[370px] lg:max-w-[470px] md:h-full lg:max-h-[536px]'>
           <Image
             className='w-full h-full object-cover'
             src='/IMG_1848.JPG'
@@ -17,8 +25,8 @@ export default function About() {
             alt='A picture of Kleopatra with a smile on her face'
           />
         </figure>
-        <article className={`flex flex-col gap-6 md:gap-10 md:ml-5`}>
-          <div>
+        <article className='flex flex-col gap-6 md:gap-10 md:ml-5'>
+          <div className='lg:max-h-[626px] lg:max-w-[319px] overflow-hidden font-normal text-xl leading-7'>
             <h2 className='my-3 mb-5'>Hello!</h2>
             <p>
               I am a UX/UI designer based in Stockholm. Passionate about
