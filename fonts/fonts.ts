70f158b9aaf48b11d86a2f627508b001418f8ffb
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2024-08-19 16:07:09 +0200

    create file for all the fonts to export within the app

---
+++ fonts/fonts.ts
@@ -0,0 +1,11 @@
+import { Space_Mono, Work_Sans } from 'next/font/google';
+
+export const spaceMono = Space_Mono({
+  subsets: ['latin'],
+  weight: ['700', '400'],
+});
+
+export const workSans = Work_Sans({
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
+});
